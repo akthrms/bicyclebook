cebfc70c93dae61b139298f7f614ac4ddee099d4
akthrms <akt.hrms@gmail.com>
2021-10-02 02:32:21 +0900

    update

---
+++ ch07/toy-vec/examples/toy_vec_02.rs
@@ -0,0 +1,16 @@
+use toy_vec::ToyVec;
+
+fn main() {
+    let _e: Option<&String>;
+
+    {
+        let mut v = ToyVec::new();
+
+        v.push("Java Finch".to_string());
+        v.push("Budgerigar".to_string());
+
+        _e = v.get(1);
+    }
+
+    // assert_eq!(e, Some(&"Budgerigar".to_string()));
+}
